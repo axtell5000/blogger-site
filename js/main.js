e095164d79e5db483801f9bebe898af91936059f
Stephen Axtell <axtell.stephen@gmail.com>
2020-01-28 11:34:50 +0200

    added a scroll to top script

--- js/main.js
+++ js/main.js
@@ -16,4 +16,11 @@ $(document).ready(function() {
 		nav: true,
 		navText: [$('.owl-navigation .owl-nav-prev'), $('.owl-navigation .owl-nav-next')]
 	});
+
+	//scroll to top 
+	$('.move-up span').click(function() {
+		$('html, body').animate({
+			scrollTop: 0
+		}, 1000);
+	});
 });
